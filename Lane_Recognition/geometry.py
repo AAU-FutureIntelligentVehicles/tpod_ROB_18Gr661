17740a999e096c2600eba2e39e106db9f49347c9
Simon Kaihoj <skaihoj@yahoo.dk>
2018-04-30 15:39:51 +0200

    Point cloud lookup rounding

--- Lane_Recognition/geometry.py
+++ Lane_Recognition/geometry.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+#Takes angles and returns rotation matrix
 def rot(r, p, y):
-	
 	Rx = np.array([[1, 0, 0], [0, np.cos(r), -np.sin(r)], [0, np.sin(r), np.cos(r)]])
 	Ry = np.array([[np.cos(p), 0, -np.sin(p)], [0, 1, 0], [np.sin(p), 0, np.cos(p)]])
 	Rz = np.array([[np.cos(y), -np.sin(y), 0], [np.sin(y), np.cos(y), 0], [0, 0, 1]])
@@ -11,27 +11,32 @@ def rot(r, p, y):
 	a[3:, 3:] = b
 	return a
 
-
-
-
+#not used
+#Previously used to load image and point cloud data
 def load_data(filename):
 	return np.load("training/{0}.npy".format(filename)).reshape((720, -1, 6))
 
+	
+#not used
+#previously for displaying color and depth data together
 def display_depth(mat, scaling):
 	plt.imshow(mat[..., (0, 1, 4)]/[256 ,256, scaling])
 	plt.show()
 
+#takes a point cloud, a set of Euler angles in radians and a threshold in millimeters
+#returns a binary image showing the area of the point cloud that is below this level after rotation
 def is_road(img, rot_=(0.3, 0, 0), thresh = 1700):
 	rot_mat = rot(rot_[0], rot_[1], rot_[2]) 
 	rot_img = np.nan_to_num(img).dot(rot_mat)
 	road = (rot_img[..., 1] < thresh)*1
 	return road
 	
-	
+#takes a contour and a point cloud from the same image, and looks up the points from the contour
+#Returns the world coordinates of the points 
 def pcl_lookup (contour, point_cloud, rot_ =(0.3, 0, 0)):
 	rot_mat = rot(rot_[0], rot_[1], rot_[2]) 
 	rot_pc= np.nan_to_num(point_cloud).dot(rot_mat)
 	new_contours = []
 	for point in contour:
-		new_contours.append([[rot_pc[point[0,0],point[0,1], 0], rot_pc[point[0,0],point[0,1], 2]]]) 
+		new_contours.append([[int(rot_pc[point[0,0],point[0,1], 0]), int(rot_pc[point[0,0],point[0,1], 2)]]]) 
 	return new_contours
