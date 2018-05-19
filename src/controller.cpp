eebc98caefcb5c913456ac4fb8f4f515dae6abd8
ZzimonNanoq <scalli15@student.aau.dk>
2018-05-19 15:42:37 +0200

    fixed gain in file and added some comments

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-#define K 0.4   //gain
+#define K 0.5   //gain
 
 //global variables that get updated when new information from the subsribers are gathered
 float turningAngle = 0;
@@ -18,18 +18,18 @@ std_msgs::Float64 accelerator_position;
 
 //Callback function updating global variables from the path planner
 void updatePosition_cb(geometry_msgs::Pose2D point){
-	turningAngle = point.theta;											//This is Theta Error. Turning angle received from the path point package.
-	accelerator_position.data = 1;										//This is what we send to the golf cart accellerator. Having it in the callback makes sure we only drive, when we have started receiving points.
+	turningAngle = point.theta;														//This is Theta Error. Turning angle received from the path point package.
+	accelerator_position.data = 1;												//This is what we send to the golf cart accellerator. Having it in the callback makes sure we only drive, when we have started receiving points.
 }
 
 //Callback function updating the global variable if we are going to collide and we shoule brake.
 void updateAvoidance_cb(std_msgs::Int8 isAvoidanceCallBack){
     if (isAvoidanceCallBack.data == 1) {
-        isAvoidance = 1;												//If we receive 1 from the avoidance system we want to brake. Changing isAvoidance to 1 enters the loop for braking in main.
+        isAvoidance = 1;																//If we receive 1 from the avoidance system we want to brake. Changing isAvoidance to 1 enters the loop for braking in main.
     }
 
 	else if (isAvoidanceCallBack.data == 0){
-		isAvoidance = 0;												//When the obstacle has been removed we want to drive again. Here we set isAvoidance to 0 to enter the loop for driving.
+		isAvoidance = 0;																		//When the obstacle has been removed we want to drive again. Here we set isAvoidance to 0 to enter the loop for driving.
 	}
 }
 
@@ -57,11 +57,11 @@ int main(int argc, char **argv){
 
 	while(ros::ok()){
 
-		if (turningAngle != 1000){									//Loop for driving when no obstacle is in front of us
+		if (turningAngle != 1000){													//Loop for driving when no obstacle is in front of us
 			//convert the angle to ticks
 			float beta = turningAngle * K * 43408.5889;
 
-			//safety stuff so we dont turn the steering wheel more than the car is physically capable of
+			//safety stuff so we dont try to turn the steering wheel more than the car is physically capable of
 			if (beta >= 22000){
 				beta = 22000;
 			}
@@ -69,23 +69,23 @@ int main(int argc, char **argv){
 				beta = -22000;
 			}
 
-			cout << beta << endl;												//Outputting what we want to turn
+			cout << beta << endl;															//Outputting what we want to turn
 
-			turn_ticks.data = beta;												//This is what we publish to the steering wheel
-			brake_position.data = 2.4;											//When brake is set to 2.4 we are not braking
+			turn_ticks.data = beta;														//This is what we publish to the steering wheel
+			brake_position.data = 2.4;												//When brake is set to 2.4 we are not braking
 
 			//Here we do the actual publishing
 			set_steering_angle_pub.publish(turn_ticks);
 			set_accelerator_position_pub.publish(accelerator_position);
 			set_brake_position_pub.publish(brake_position);
 
-			ros::spinOnce();													//updates subsribers
+			ros::spinOnce();																	//updates subsribers
 
-			usleep(10000);														//Waits some time
+			usleep(10000);																		//Waits some time
 		}
 
-		else if (turningAngle == 1000){												//This is the loop for braking, when an obstacle is in front of us.
-		brake_position.data = 0;												//we want to publish 0 to the brake = we start braking maximum
+		else if (turningAngle == 1000){											//This is the loop for braking, when no usable path points are received
+		brake_position.data = 0;														//we want to publish 0 to the brake = we start braking maximum
 		accelerator_position.data = 0;											//we want to send 0 to the accelerator = we stop applying speed
 
 		//Here we do the actual publishing for braking
@@ -93,8 +93,6 @@ int main(int argc, char **argv){
 		set_accelerator_position_pub.publish(accelerator_position);
 
 		ros::spinOnce();														//updates subsribers
-
 		}
-
 	}
 }
