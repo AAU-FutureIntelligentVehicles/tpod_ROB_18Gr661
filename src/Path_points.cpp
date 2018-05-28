e853b02b2d748c7b42d35d5aef36b8b4ecb6d9db
Iosif Beres <37441483+IosifB@users.noreply.github.com>
2018-05-28 17:30:42 +0200

    Update Path_points.cpp
    clean

--- src/Path_points.cpp
+++ src/Path_points.cpp
@@ -7,8 +7,8 @@
 #include <string>
 #include "stdio.h"
 
-#define LOWER_DIST 1.6                //The distance the cart look ahead is between 1.6 and 2 meter
-#define UPPER_DIST 2                  //not used in current implementation
+#define LOWER_DIST 3.2                //The distance the cart look ahead is between 3.2 and 3.6 meters
+#define UPPER_DIST 3.6                  //not used in current implementation
 #define WHEEL_ANGLE 1.57         //90 degrees viewing angle
 #define WHEEL_BASE 1.64               //The wheelbase length
 
@@ -31,10 +31,7 @@ public:
     //whenever a point is received, this function checks if a point on the path is within the search space, if it is it calculated theta_error and sends it to the controller.
     void Path_Point_Pub_CB(const geometry_msgs::Pose2D path_points)
     {
-        //if(path_points.empty())
-        //{
-        //    return;
-        //}
+      
         float alpha =  atan2(path_points.y,path_points.x)  ;                            //formula for finding an angle between two vectors
 
         if(path_points.x==0&&path_points.y==0)
@@ -42,52 +39,15 @@ public:
             solution.theta = 1000;
             point_pub.publish(solution);
         }
-        else //if(alpha <= WHEEL_ANGLE/2) //alpha <= VIEWING_ANGLE/2){
+        else 
         {
             solution.theta = alpha;
             point_pub.publish(solution);
         }
         return;
     }
-                // set the solution
-                //solution = path_points;
-                //float determinant = car_vecx * p_vecy;// - car_vecy * p_vecx;                      //checking if the aiming vector is on the right hand side or left hand side. (should the cart turn right or left)
-                //if (determinant < 0)
-                //{
-                //    solution.theta = -alpha;
-                //}
-                //else
+                
 
-
-
-        //    ++itter; // try the next point.
-        //}
-
-        //        std::cout<<"x =: "<<path_points.x<<"y ="<<path_points.y<<std::endl;
-        //        return;}
-
-        //for (auto itter = path_points)
-        //{
-        //calculate angle between cart vector and aiming vector
-        //float dotprod = car_vecx * p_vecx; //+ car_vecy * p_vecy;                           //Finding the dotproduct between the two vectors
-        //float norm_tpod = sqrt(car_vecx * car_vecx); //+ car_vecy * car_vecy);               //findin the length of the cart vector
-        //float norm_itter = sqrt(p_vecx * p_vecx + p_vecy * p_vecy);                         //findin the length of the Aiming vector
-        //float alpha = acos(dotprod/(norm_tpod*norm_itter));
-
-        //float car_vecx = 1;                 //cos(theta)= 1
-        //  float car_vecy = sin(theta) ;     because theta=0 and cart direction doesn't matter this cancels out.
-        //float p_vecx = cos(path_points.x) ;
-        //float p_vecy = sin(path_points.y) ;
-        // is the angle within our viweing angle
-    // we only get here if we went through the whole path
-    // if there is only one point left we keep publishing a last point 1 m away from the last point
-    // to ensure that the car drives all the way to the last path point.
-
-
-    //else {
-    //    ROS_INFO("There doesnt seem to be any path points within the bounds"); // if anything fails
-    //    std::cout<<"\nThe next point is "<<path_points[1].x<<","<<path_points[1].y<<" | "<<path_points[1].theta<<std::endl;
-    //}
 
 
 
